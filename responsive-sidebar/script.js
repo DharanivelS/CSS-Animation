19d4e36755b2eb214237305ae37f5065cf71afe9
Dharanivel Vicky <Rockv@DESKTOP-AB03V42.(none)>
2020-08-14 02:56:44 +0530

    modified responsivez sidebar

--- responsive-sidebar/script.js
+++ responsive-sidebar/script.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
     $(".toggle").click(function(){
        $(".navigation").toggleClass("active");
        $(this).toggleClass("active");
+       $(".para").toggleClass("active");
     });
 });
